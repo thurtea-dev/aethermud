abaadc462da899c1d94d04953b1dab8fa61c6a49
Thurtea <itsthurtea@gmail.com>
2026-07-23 13:25:58 -0300

    commit

--- nightmare3_fluffos_v2/lib/domains/Praxis/kataan_join.c
+++ nightmare3_fluffos_v2/lib/domains/Praxis/kataan_join.c
@@ -1,82 +1,39 @@
 #include <std.h>
-int go_down();
+#include <rooms.h>
 
 inherit ROOM;
 
-void init() {
-    ::init();
-    add_action("become", "become");
-    add_action("preview","preview");
-}
-
 void create() {
     ::create();
-set_properties( (["no castle":1,"light":2,"indoors":1,"no castle":1]));
-    set("short", "cavern of the kataans");
+    set_property("light", 2);
+    set_property("indoors", 1);
+    set_property("no castle", 1);
+    set_short("the old cavern of the kataans");
     set_long(
-        "You are in a dark underground cavern just north of Praxis. "
-"Deep, intricate murals and symbols are carved into the walls of the "
-     "cave. Down deeper in the cavern is a passage protected "
-       "by a shimmering %^BLUE%^blue%^RESET%^ light. "
-        "<preview> will tell you about becoming a Kataan. ");
-    set_items(
-	(["mural" : "It is very old and depicts gruesome cult practices.",
-	  "murals" : "They are quite ancient and are made up of "
-	    "pictures depicting bizarre rituals.",
-	  "light" : "You are certain that it is a field which allows "
-	    "only kataans to pass.",
-	  "cavern" : "The cavern almost pulses with hidden evil."]) );
-    set_exits(
-	      (["up" : "/domains/Praxis/forest3"]) );
-    add_exit("down","/domains/Praxis/kataan_hall",(:go_down:));
+        "Deep, intricate murals and symbols are carved into the walls of\n"
+        "this underground cavern just north of Praxis, faded and half\n"
+        "crumbled now. Whatever coven once trained here is long gone. A\n"
+        "passage leads down into the cavern they left behind.");
+    set_items( ([
+        "mural"  : "Old and worn, depicting rituals no one remembers anymore.",
+        "murals" : "Ancient and crumbling, showing bizarre practices from an "
+            "order long since gone.",
+        "light"  : "A faint %^BLUE%^blue%^RESET%^ light lingers here, worn thin.",
+        "cavern" : "The cavern feels hollow now, whatever it once held long "
+            "since departed."
+    ]) );
+    set_exits( ([
+        "up" : "/domains/Praxis/forest3"
+    ]) );
 }
 
-int preview() {
-  if((string)this_player()->query_class() != "child") {
-    write("This is not for you.");
-    return 1;
-  }
-  say(this_player()->query_cap_name()+" seeks to learn about "
-      "kataans.", this_player());
-  write("Welcome, child! ");
-  write("Kataans are an ancient coven of evil, magical warriors skilled "
-        "in the use of projectiles and stealth. They are careful to do "
-        "no acts which might be considered good. Instead, the use the "
-        "powers given to them by their demon masters to terrorize good "
-        "beings wherever they may be.\n"
-        "To become a kataan, type <become kataan>\n");
-    return 1;
+void init() {
+    ::init();
+    add_action("go_down", "down");
 }
 
-int become(string str) {
-    if(!str) {
-	notify_fail("Become what?\n");
-	return 0;
-    }
-    if(str != "kataan") {
-	notify_fail("You cannot become that here.\n");
-	return 0;
-    }
-    if((string)this_player()->query_class() != "child") {
-	write("You are much too old to start learning our ways now!");
-	return 1;
-    }
-    write("The Master Kataan initiates you into the class of kataans.");
-    say(this_player()->query_cap_name()+" becomes a kataan.", this_player());
-    this_player()->set_class("kataan");
-    this_player()->setenv("TITLE", "$N the pebble tosser");
-    this_player()->init_skills("kataan");
-    this_player()->setenv("start", "/domains/Praxis/kataan_hall");
+int go_down(string str) {
+    this_player()->move("/domains/Praxis/kataan_hall");
+    write("You descend into the cavern below.\n");
     return 1;
 }
-
-int go_down() {
-    if((string)this_player()->query_class() != "kataan") {
-	write("You cannot penetrate the force field that blocks the passage.");
-	say(this_player()->query_cap_name()+" tries to get into the kataan's sanctuary, but fails.", this_player());
-  	return 0;
-    }
-    return 1;
-}
-
-
